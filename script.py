dd5ad9fbe0cee0938ec0be6fa85b5b6caf5dd250
[David Walters] <[davidwalters47@hotmail.co.uk]>
2023-01-09 19:13:43 +0000

    Added test objects

---
+++ script.py
@@ -0,0 +1,2 @@
+destinations = ["Paris, France", "Shanghai, China", "Los Angeles, USA", "Sao Paulo, Brazil", "Cairo, Egypt"]
+test_traveler = ["Erin Wilkes", "Shanghai, China", ["historical site", "art"]]
