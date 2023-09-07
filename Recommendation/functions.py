fd55fd96fba2178dce4339dfae2b91bff456e34e
[David Walters] <davidwalters47@hotmail.co.uk>
2023-09-07 18:45:33 +0100

    return user suggestions based on attractions

--- Recommendation/functions.py
+++ Recommendation/functions.py
@@ -18,10 +18,21 @@ def retrieve_attractions(destinations):
     user_choices = []
     attractions = suggest_attractions(destinations)
     another_choice = True
-    while another_choice and len(user_choices) <= 3:
+    while another_choice == True and len(user_choices) < 3:
         attraction = int(input("Please enter the number of the attraction that appeals to you: "))
         user_choices.append(attractions[attraction-1])
-        ask_again = input("Would you like to make another selection? Yes/No: ").lower()
-        if ask_again[0] == "n":
+        if len(user_choices) < 3:
+            ask_again = input("Would you like to make another selection? Yes/No: ").lower()
+        if ask_again[0] != "y":
             another_choice = False
     return user_choices
+
+def destinations_from_attractions(destinations):
+    user_destinations = []
+    user_choices = retrieve_attractions(destinations)
+    for destination in destinations.array:
+        for item in destinations.retrieve(destination[0]).attractions:
+            if item in user_choices:
+                if destination not in user_destinations:
+                    user_destinations.append(destination)
+    return user_destinations
